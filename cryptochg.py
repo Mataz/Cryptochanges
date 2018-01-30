c82a9559029f1ebe0a46ea4790e4e12c2e82abf5
Thomas Mahieux <thomas.mahieux@gmail.com>
2018-01-30 15:23:21 +0100

    Update cryptochg.py
    Switch to the coincap.io's API + included Market Caps

--- cryptochg.py
+++ cryptochg.py
@@ -6,7 +6,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from pandas.tools.plotting import table
 
-url = 'https://api.coinmarketcap.com/v1/ticker/'
+url = 'http://coincap.io/front'
 data = requests.get(url).json()
 
 if not os.path.exists('df_image'):
@@ -16,36 +16,34 @@ os.chdir('df_image')
 
 
 def top_increase():
-    ordered_data = sorted(data, key=lambda k:
-    (float(k['percent_change_24h']), (k['percent_change_7d'])), reverse=True)[:5]
+    top100_data = sorted(data, key=lambda k: (float(k['mktcap'])), reverse=True)[:100]
+    ordered_data = sorted(top100_data, key=lambda k: (float(k['cap24hrChange'])), reverse=True)[:5]
     raw_data_increase = {}
 
     for currency in ordered_data:
-        rank = currency['rank']
-        name = currency['name']
-        percent_change_24h = currency['percent_change_24h']
-        percent_change_7d = currency['percent_change_7d']
-        price_usd = float(currency['price_usd'])
-        price_usd = f'{price_usd:.2f}'
+        name = currency['long']
+        market_cap = float(currency['mktcap'])
+        market_cap_rounded = f'{market_cap:,.2f}'
+        percent_change_24 = currency['cap24hrChange']
+        price = float(currency['price'])
+        price_rounded = f'{price:.2f}'
 
-        raw_data_increase.setdefault('Rank', [])
-        raw_data_increase['Rank'].append(rank)
         raw_data_increase.setdefault('Name', [])
         raw_data_increase['Name'].append(name)
-        raw_data_increase.setdefault('Change last 24h (%)', [])
-        raw_data_increase['Change last 24h (%)'].append(percent_change_24h)
-        raw_data_increase.setdefault('Change last 7d (%)', [])
-        raw_data_increase['Change last 7d (%)'].append(percent_change_7d)
+        raw_data_increase.setdefault('Market Cap(USD)', [])
+        raw_data_increase['Market Cap(USD)'].append(market_cap_rounded)
+        raw_data_increase.setdefault('%24hr', [])
+        raw_data_increase['%24hr'].append(percent_change_24)
         raw_data_increase.setdefault('Price(USD)', [])
-        raw_data_increase['Price(USD)'].append(price_usd)
-        # print(raw_data)
+        raw_data_increase['Price(USD)'].append(price_rounded)
+        
 
-    df = pd.DataFrame(raw_data_increase, columns=['Rank', 'Name', 'Change last 24h (%)', 'Change last 7d (%)', 'Price(USD)'])
+    df = pd.DataFrame(raw_data_increase, columns=['Name', 'Market Cap(USD)', '%24hr', 'Price(USD)'])
 
     print(df.to_string(index=False))
 
     # set fig size
-    fig, ax = plt.subplots(figsize=(9.5, 4))
+    fig, ax = plt.subplots(figsize=(6.8, 2))
     # no axes
     ax.xaxis.set_visible(False)
     ax.yaxis.set_visible(False)
@@ -55,9 +53,9 @@ def top_increase():
     tab = table(ax, df, rowLabels=['']*df.shape[0], loc='center')
     # set font manually
     tab.auto_set_font_size(False)
-    tab.set_fontsize(7.6)
+    tab.set_fontsize(8.8)
     # set scale
-    tab.scale(1, 3.5)
+    tab.scale(1, 1.8)
     # save the result
     plt.savefig('cryptopinc.png')
 
@@ -76,12 +74,12 @@ api = tp.API(auth)
 
 
 while True:
-    for df_image in os.listdir('.'):
-        api.update_with_media(df_image, status='Here are the top 5 coins that got their '
-                                               'value increased the most in the last '
-                                               '24 hours. #Cryptocurrency #Crypto')
+    for image in os.listdir('.'):
+        api.update_with_media(image, status='Here are the top 5 coins that got their '
+                                            'value increased the most during the '
+                                            'last 24 hours. #Cryptocurrency #Crypto #Altcoins')
 
-        time.sleep(21600)
+        time.sleep(43200)
 
 
 
